fd0071bbfb86119d18da07d1ef4fc4ae542de5e6
Ashwin Umadi <ashwinumadi@Ashwins-MacBook-Pro.local>
2025-05-26 13:02:54 -0600

    small epoch test

--- blanca_mini_set.sh
+++ blanca_mini_set.sh
@@ -4,7 +4,7 @@
 #SBATCH --time=24:00:00
 #SBATCH --partition=blanca-curc-gpu
 #SBATCH --gres=gpu:1
-#SBATCH --output=run_code-%j.out
+#SBATCH --output=small_epoch100_res-%j.out
 #SBATCH --mail-type="ALL"
 #SBATCH --mail-user="asum8093@colorado.edu"
 
@@ -48,38 +48,38 @@ device=0
 #                             --type_vocab_file "./data/small_processed_data/types.txt" \
 #                             --batch 4
 
-python3 eval.py \
-                             --model_dir "./model_dir/small_model_100_epoch/" \
-                             --eval_data_path "./data/bins_processed_data/test_split_q1_processed.json" \
-                             --type_vocab_file "./data/bins_processed_data/types.txt" \
-                             --batch 4
+#python3 eval.py \
+#                             --model_dir "./model_dir/small_model_100_epoch/" \
+#                             --eval_data_path "./data/bins_processed_data/test_split_q1_processed.json" \
+#                             --type_vocab_file "./data/bins_processed_data/types.txt" \
+#                             --batch 4
 
-echo "=============== DDEV 1 ========================="
+#echo "=============== DDEV 1 ========================="
 
 
-python3 eval.py \
-                             --model_dir "./model_dir/small_model_100_epoch/" \
-                             --eval_data_path "./data/bins_processed_data/test_split_q2_processed.json" \
-                             --type_vocab_file "./data/bins_processed_data/types.txt" \
-                             --batch 4
+#python3 eval.py \
+#                             --model_dir "./model_dir/small_model_100_epoch/" \
+#                             --eval_data_path "./data/bins_processed_data/test_split_q2_processed.json" \
+#                             --type_vocab_file "./data/bins_processed_data/types.txt" \
+#                             --batch 4
 
-echo "=============== DDEV 2 ========================="
+#echo "=============== DDEV 2 ========================="
 
-python3 eval.py \
-                             --model_dir "./model_dir/small_model_100_epoch/" \
-                             --eval_data_path "./data/bins_processed_data/test_split_q3_processed.json" \
-                             --type_vocab_file "./data/bins_processed_data/types.txt" \
-                             --batch 4
+#python3 eval.py \
+#                             --model_dir "./model_dir/small_model_100_epoch/" \
+#                             --eval_data_path "./data/bins_processed_data/test_split_q3_processed.json" \
+#                             --type_vocab_file "./data/bins_processed_data/types.txt" \
+#                             --batch 4
 
-echo "=============== DDEV 3 ========================="
+#echo "=============== DDEV 3 ========================="
 
-python3 eval.py \
-                             --model_dir "./model_dir/small_model_100_epoch/" \
-                             --eval_data_path "./data/bins_processed_data/test_split_q4_processed.json" \
-                             --type_vocab_file "./data/bins_processed_data/types.txt" \
-                             --batch 4
+#python3 eval.py \
+#                             --model_dir "./model_dir/small_model_100_epoch/" \
+#                             --eval_data_path "./data/bins_processed_data/test_split_q4_processed.json" \
+#                             --type_vocab_file "./data/bins_processed_data/types.txt" \
+#                             --batch 4
 
-echo "== End of Eval code =="
+#echo "== End of Eval code =="
 
 python3 result.py --dev "./model_dir/small_model_100_epoch/Evaluation_dev_processed.json" \
                    --test "./model_dir/small_model_100_epoch/Evaluation_test_split_q1_processed.json" \
